789820635671f01305fb2509dcd65b0e95ba4b6f
Abhishek Humagain <abhishek.humagain@innovatetech.co>
2022-04-10 07:20:13 +0545

    initial commit

---
+++ CourseWorkSampleAuth/DBContext/ApplicationDBContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using CourseWorkSampleAuth.Models;
+using CourseWorkSampleAuth.Models.ViewModels;
+
+namespace CourseWorkSampleAuth.DBContext
+{
+    public class ApplicationDBContext: IdentityDbContext<IdentityUser>
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+        public DbSet<CourseWorkSampleAuth.Models.ViewModels.UserDetailsViewModel> UserDetailsViewModel { get; set; }
+    }
+}
